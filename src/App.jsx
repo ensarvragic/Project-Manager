1d4905a478adf07c5c134a03d28f8058b1bff6ac
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-22 17:49:49 +0100

    completed the tailwind css for the sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import ProjectSideBar from "./components/ProjectSideBar";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <ProjectSideBar />
-    </>
+    </main>
   );
 }
 
