6444bf0ae0f3565d92c311dfdb5b261c6d51ca29
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-24 16:18:36 +0100

    collecting user input with refs

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   });
 
   const handleStartAddProject = () => {
-    setProjectsState(prevState => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: null,
@@ -18,10 +18,34 @@ function App() {
     });
   };
 
+  const handleAddProject = (projectData) => {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return{
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    });
+  };
+
+  console.log(projectsState)
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar onStartAddProject={handleStartAddProject}/>
-      <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
