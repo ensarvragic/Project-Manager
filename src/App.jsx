85af9f288388c0f0cc7b194203f1814e28d32866
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-24 15:43:07 +0100

    made a reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ProjectSideBar from "./components/ProjectSideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
-      <NewProject />
+      <NoProjectSelected/>
     </main>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function Input({ label, textarea, ...props }) {
-    const classes = 'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
 
   return (
     <p className="flex flex-col gap-1 my-4">
@@ -9,10 +10,7 @@ export default function Input({ label, textarea, ...props }) {
         {label}
       </label>
       {textarea ? (
-        <textarea
-          className={classes}
-          {...props}
-        />
+        <textarea className={classes} {...props} />
       ) : (
         <input className={classes} {...props} />
       )}
