3c5a2efc3c6a93a994b463f71de9a93ce3b56516
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-24 15:48:55 +0100

    managing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectSideBar from "./components/ProjectSideBar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return{
+        selectedProjectId: null,
+      }
+    });
+  };
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
-      <NoProjectSelected/>
+      <NoProjectSelected />
     </main>
   );
 }
