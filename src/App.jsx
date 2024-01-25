f0e1442ca43c7b27c9bc381b1a75438f9df23360
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-25 15:58:08 +0100

    styled selected projects

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,15 @@ function App() {
     });
   };
 
+  const handleCancelProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   const handleAddProject = (projectData) => {
     setProjectsState((prevState) => {
       const newProject = {
@@ -38,7 +47,7 @@ function App() {
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject}/>;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
