a6ca32673df0cc7c3c7d1be48e126048684f7ca2
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-25 15:47:38 +0100

    styled button and components

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
+import Button from "./Button";
 
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
-  const dialog = useRef();  
+  const dialog = useRef();
   useImperativeHandle(ref, () => {
-    return { 
-        open() {
-            dialog.current.showModal();
-        } 
+    return {
+      open() {
+        dialog.current.showModal();
+      },
     };
   });
 
   return createPortal(
-    <dialog ref={dialog}>
-        {children}
-        <form method="dialog">
-            <button>{buttonCaption}</button>
-        </form>
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
     </dialog>,
     document.getElementById("modal-root")
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,6 @@ import Input from "./Input";
 import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
-
   const modal = useRef();
   const title = useRef();
   const description = useRef();
@@ -15,13 +14,13 @@ export default function NewProject({ onAdd }) {
     const enteredDueDate = dueDate.current.value;
 
     if (
-      enteredTitle.trim() === '' ||
-      enteredDescription.trim() === '' ||
-      enteredDueDate.trim() === '' 
-      ) {
-        modal.current.open();
-        return;
-      }
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -32,33 +31,35 @@ export default function NewProject({ onAdd }) {
 
   return (
     <>
-    <Modal ref={modal} buttonCaption='Okey'>
-      <h2>Invalid Input</h2>
-      <p>Oops ... looks like you forgot to enter a value.</p>
-      <p>Please make sure you provide a valid value for every  input field.</p>
-    </Modal>
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+      <Modal ref={modal} buttonCaption="Okey">
+        <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">
+          Invalid Input
+        </h2>
+        <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+        <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
     </>
   );
 }
